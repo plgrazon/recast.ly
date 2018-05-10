c620505773556d22c8c4f5104809717ee03e6ea3
plgrazon <plgrazon@gmail.com>
2018-05-09 20:26:49 -0700

    Complete basic functionality for recast.ly.

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,10 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+    {
+      props.videos.map((item, index) => {
+          return <VideoListEntry onListItemClick={props.onListItemClick} video={item} key={index}/>;
+      })
+    }
   </div>
 );
 
